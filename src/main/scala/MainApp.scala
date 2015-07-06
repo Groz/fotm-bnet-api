963498ef8446d02a22a40328dae0ea04ba96a84c
Tagir Magomedov <tmagomedov@twitter.com>
2015-07-06 14:15:56 -0700

    Update dispatch version to 0.11.3. Update default App setting

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -1,13 +1,14 @@
 import info.fotm.api._
 import info.fotm.api.models.{Twos, Appearance, PvP}
 import info.fotm.api.regions.Europe.{RuRU, EsES}
+import info.fotm.api.regions.US.EnUS
 import info.fotm.api.regions._
 import scala.concurrent.Await
 import scala.concurrent.duration._
 import play.api.libs.json.{JsValue, Json}
 
 object MainApp extends App {
-  val api = new BattleNetAPI(Europe, "vntnwpsguf4pqak7e8y7tgn35795fqfj", RuRU).WoW
+  val api = new BattleNetAPI(US, "vntnwpsguf4pqak7e8y7tgn35795fqfj", EnUS).WoW
 
   val jsonFuture = api.leaderboard(Twos)
 
